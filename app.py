287b2ef2172e3adceb9afaba357ad5960204bfe3
Ahmad <62955737+No0b-master@users.noreply.github.com>
2021-03-25 15:42:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+unique = [] 
+numbers=[] 
+n = int(input('No of elements : ')) 
+for i in range(0,n): 
+  element=int(input(">>>")) 
+  numbers.append(element)
+
+def duplicate(numbers): 
+  for number in numbers: 
+    if number not in unique: 
+      unique.append(number) 
+      return unique 
+    print(f'list after removing duplicates : {duplicate(numbers)}')
+
